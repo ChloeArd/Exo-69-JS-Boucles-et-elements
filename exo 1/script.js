d9cc769abe2d29f1bf1268568af16909bcbafe71
ChloeArd <chloe.ardoise@gmail.com>
2020-12-03 09:22:36 +0100

    Exercice 69

--- exo 1/script.js
+++ exo 1/script.js
@@ -1,5 +1,4 @@
 let identifiant = document.getElementById('liste-commissions');
         let newListe = document.createElement('li');
         newListe.innerHTML = "nesquik ";
-
-        document.getElementById('liste-commissions').appendChild(newListe);
+        identifiant.append(newListe);
